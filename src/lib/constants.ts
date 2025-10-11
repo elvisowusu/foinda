d95fc546487bc77735d63a9cf99418c4f6d68ba8
elvisowusu <elvisgyasiowusu24@gmail.com>
2025-10-11 01:15:20 +0000

    added navbar

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -4,5 +4,4 @@ export const navLinks: NavLink[] = [
     { link: "/", text: "Home" },
     { link: "/about-us", text: "About Us" },
     { link: "/contact-us", text: "Contact Us" },
-    { link: "/partners", text: "Partners" },
   ];
